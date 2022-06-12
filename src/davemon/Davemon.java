3dbf70f8f4313722444e4f1e0ec8b7e0518918d1
David <davidricardo0219@gmail.com>
2022-06-12 04:23:05 -0400

    fixed window scaling

--- src/davemon/Davemon.java
+++ src/davemon/Davemon.java
@@ -4,6 +4,7 @@ package davemon;
 public class Davemon {
 
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1");
         JFrame jf = new JFrame();
         jf.show();
     }
